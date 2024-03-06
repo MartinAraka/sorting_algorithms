87002b52488eeece5b6e487d18b3f73916c52acd
Martin Araka <arakamartin@gmail.com>
2024-03-06 05:28:19 -0800

    Add big O notation

---
+++ 0-bubble_sort.c
@@ -0,0 +1,9 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts integer in ascending order
+ * @array: array to be sorted
+ * @size: no. of elements in the integer array
+ */
+
+void bubble_sort(int *array, size_t size)
